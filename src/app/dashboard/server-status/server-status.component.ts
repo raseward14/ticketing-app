e1260b4c25622dcf5fb0f443a5549b630a87868b
raseward14 <raseward14@gmail.com>
2025-10-09 21:29:36 -0600

    removed unnecessary wrappers with :host selector - also cleans up the DOM

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
   currentStatus = 'online';
