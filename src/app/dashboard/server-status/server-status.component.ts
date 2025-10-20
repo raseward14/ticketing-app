e4a110cd7eb6316d912df2df97dbcb9314afaa61
raseward14 <raseward14@gmail.com>
2025-10-20 11:29:34 -0600

    added logic to randomly change server status every 5 seconds to the serverStatus component

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
+
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+
+  constructor() {
+    setInterval(() => {
+      const rndm = Math.random(); // 0 - 1
+
+      if (rndm < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rndm < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      };
+    }, 5000)
+  }
 }
